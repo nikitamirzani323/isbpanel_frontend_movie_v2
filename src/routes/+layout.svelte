003e78d41ecb021f5c44f2334fdd7f49ab9f798f
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2023-06-09 20:17:42 +0700

    update navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
     import "../app.css";
     import Navbar from "./Navbar.svelte";
 </script>
-<Navbar />
-<main class="container mx-auto lg:px-2 text-base-content xl:mt-7 max-w-screen-xl bg-opacity-60 pb-5 xl:pb-5">
+<main class="container mx-auto lg:px-1 xl:mt-0 max-w-screen-xl pb-2 ">
+    <Navbar />
+</main>
+<main class="container mx-auto lg:px-2 text-base-content xl:mt-2 max-w-screen-xl bg-opacity-60 pb-5 xl:pb-5">
     <slot />
 </main>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -3,52 +3,23 @@
 
     $: routeId = $page.route.id;
 </script>
-<div class="navbar bg-base-200 mb-2">
+<div class="navbar bg-base-100">
     <div class="navbar-start">
-        <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Home</a></li>
-            <li>
-                <a>Genre</a>
-                <ul class="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-                </ul>
-            </li>
-            <li><a>Series</a></li>
-            </ul>
-        </div>
-        <a href="/" class="btn btn-ghost w-1/4">
-            <img width="50%" src="https://isbfilm.xyz/isbfilm.png" alt="">
-        </a>
+        
+        
     </div>
     <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal px-1">
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li tabindex="0">
-            <details>
-                <summary>Genre</summary>
-                <ul class="p-2">
-                    <li>
-                        <a href="/genre/action">Action</a>
-                    </li>
-                    <li>
-                        <a href="/genre/sci-fi">Sci-fi</a>
-                    </li>
-                </ul>
-            </details>
-            </li>
-            <li>
-                <a href="/series">Series</a>
-            </li>
-        </ul>
+        <a href="/" class="btn btn-ghost w-1/2">
+            <img width="100%" src="https://isbfilm.xyz/isbfilm.png" alt="">
+        </a>
     </div>
-    <div class="navbar-end">
-      <a class="btn">Button</a>
+    <div class="navbar-end p-2">
+        <div class="flex justify-end p-2 bg-base-200 w-1/2 cursor-pointer rounded-lg">
+            <p class="pr-2">Quick Search</p>
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>          
+        </div>
     </div>
 </div>
